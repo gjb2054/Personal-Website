aaaa564311b629bce1e0e45e0142c29ffda6eb4a
gjb2054 <gjb2054@rit.edu>
2023-10-02 19:39:47 -0500

    Nav Bar Work

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+
+
+function Nav() {
+    return (
+        <div>
+            <a href="/">Home</a>
+            <a href="/Experiences">Experiences</a>
+            <a href="/Projects">Projects</a>
+            <a href="/Resume">Resume</a>
+            <a href="/Contact">Contact</a>
+        </div>
+    )
+}
+
+export default Nav;
