88ba05006d3f1529a46386d8898c90014e814c4a
gjb2054 <gjb2054@rit.edu>
2023-10-02 19:33:16 -0500

    App Changes

---
+++ src/Router.js
@@ -0,0 +1,11 @@
+import { Routes, Route, BrowserRouter as Switch } from 'react-router-dom'
+
+
+function Nav() {
+    <Switch>
+        <Route path="/" element={<App />}>
+        </Route>
+    </Switch>
+}
+
+export default Router;
